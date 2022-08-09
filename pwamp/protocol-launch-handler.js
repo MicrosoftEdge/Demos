fe2b1b211a73b6ae5cb873fab9a510a72ccfd14e
Patrick Brosset <patrickbrosset@gmail.com>
2022-08-09 16:07:02 +0200

    Fixed protocol handling URL code

--- pwamp/protocol-launch-handler.js
+++ pwamp/protocol-launch-handler.js
@@ -6,6 +6,6 @@ const url = document.location.href;
 const remoteSongProtocol = new URL(url).searchParams.get('addRemoteSong');
 
 if (remoteSongProtocol) {
-  const songUrl = remoteSongProtocol.substring('web+amp:'.length);
+  const songUrl = remoteSongProtocol.substring('web+amp://'.length);
   importSongFromURL(songUrl, getSongNameFromURL(songUrl)).then(startApp);
 }
