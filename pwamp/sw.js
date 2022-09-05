a79093e84a3b92eca7dbb2b47d4f6bc99617555c
Patrick Brosset <patrickbrosset@gmail.com>
2022-09-05 14:43:21 +0200

    Better SW update and caching strategy

--- pwamp/sw.js
+++ pwamp/sw.js
@@ -1,13 +1,19 @@
-const VERSION = "v14";
+const VERSION = "v15";
 const CACHE_NAME = `pwamp-${VERSION}`;
 
+// Those are all the resources our app needs to work.
+// We'll cache them on install.
 const INITIAL_CACHED_RESOURCES = [
   "./",
   "./skins/default.css",
+  "./album-art-placeholder.png",
   "./app.js",
+  "./audio-metadata-parse-worker.js",
   "./exporter.js",
   "./file-launch-handler.js",
   "./importer.js",
+  "./index.html",
+  "./parseAudioMetadata.js",
   "./player.js",
   "./popup-polyfill.js",
   "./protocol-launch-handler.js",
@@ -18,25 +24,32 @@ const INITIAL_CACHED_RESOURCES = [
   "./store.js",
   "./utils.js",
   "./visualizer.js",
-  "./audio-metadata-parse-worker.js",
-  "./parseAudioMetadata.js",
-  "./album-art-placeholder.png"
+  "https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm"
 ];
 
-// Bust the cache when the version changes.
-for (let i = 0; i < INITIAL_CACHED_RESOURCES.length; i++) {
-  INITIAL_CACHED_RESOURCES[i] = INITIAL_CACHED_RESOURCES[i] + "?v=" + VERSION;
-}
+// Add a cache-busting query string to the pre-cached resources.
+// This is to avoid loading these resources from the disk cache.
+const INITIAL_CACHED_RESOURCES_WITH_VERSIONS = INITIAL_CACHED_RESOURCES.map(path => {
+  return `${path}?v=${VERSION}`;
+});
 
 // On install, fill the cache with all the resources we know we need.
+// Install happens when the app is used for the first time, or when a
+// new version of the SW is detected by the browser.
+// In the latter case, the old SW is kept around until the new one is
+// activated by a new client.
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    cache.addAll(INITIAL_CACHED_RESOURCES);
+    cache.addAll(INITIAL_CACHED_RESOURCES_WITH_VERSIONS);
   })());
 });
 
-// Use the activate event to delete old caches and avoid running out of space.
+// Activate happens after install, either when the app is used for the
+// first time, or when a new version of the SW was installed.
+// We use the activate event to delete old caches and avoid running out of space.
 self.addEventListener("activate", event => {
   event.waitUntil((async () => {
     const names = await caches.keys();
@@ -45,25 +58,30 @@ self.addEventListener("activate", event => {
         return caches.delete(name);
       }
     }));
+    await clients.claim();
   })());
 });
 
+// Main fetch handler.
+// A cache-first strategy is used, with a fallback to the network.
+// The static resources fetched here will not have the cache-busting query
+// string. So we need to add it to match the cache.
 self.addEventListener("fetch", event => {
   if (event.request.method === 'POST') {
     return;
   }
 
-  // On fetch, go to the network first (and cache the response), and then cache.
+  // On fetch, go to the cache first, and then network.
   event.respondWith((async () => {
     const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(`${event.request.url}?v=${VERSION}`);
 
-    try {
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
       const fetchResponse = await fetch(event.request);
       cache.put(event.request, fetchResponse.clone());
       return fetchResponse;
-    } catch (e) {
-      const cachedResponse = await cache.match(event.request);
-      return cachedResponse;
     }
   })());
 });
