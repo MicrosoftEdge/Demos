5a07c526d38ff00241b1b401d546b02b9c97f35d
Amanda Baker <amandaabaker21@gmail.com>
2023-09-11 14:10:28 -0700

    Adding Local Storage support (#1)

--- manifest-generator/app.js
+++ manifest-generator/app.js
@@ -1,6 +1,8 @@
+import { readManifestFromLocalStorage } from "./state.js";
+
 const registerServiceWorker = async () => {
   try {
-    await navigator.serviceWorker.register('sw.js');
+    await navigator.serviceWorker.register("sw.js");
     console.log('Service worker registered');
   } catch (e) {
     console.log(`Registration failed: ${e}`);
@@ -10,3 +12,7 @@ const registerServiceWorker = async () => {
 if (navigator.serviceWorker) {
   registerServiceWorker();
 }
+
+// Grab previous state from Local Storage so that progress is not lost
+// across sessions.
+readManifestFromLocalStorage();
