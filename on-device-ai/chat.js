649affc3ef0422ec4cd9b9f01d2aa005652cd468
Patrick Brosset <patrick.brosset@microsoft.com>
2024-07-15 16:47:01 +0200

    WIP local SLM samples

---
+++ on-device-ai/chat.js
@@ -0,0 +1,174 @@
+import { Init, Query, Abort } from "./main.js";
+import { marked } from "marked";
+
+const preCannedQueries = {
+  1: "Tell me about the lighthouse of Alexandria.",
+  2: "Did the lighthouse of Alexandria existed at the same time the library of Alexandria existed?",
+  3: "How did the Pharos lighthouse impact ancient maritime trade?",
+  4: "Tell me about Constantinople.",
+};
+
+const clipboardIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+</svg>`;
+
+marked.use({ mangle: false, headerIds: false });
+
+const sendButton = document.getElementById("send-button");
+const scrollWrapper = document.getElementById("scroll-wrapper");
+
+//
+// auto scroll the content area until a user scrolls up
+//
+let isAutoScrollOn = true;
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+const autoScroller = new ResizeObserver(() => {
+  if (isAutoScrollOn) {
+    scrollWrapper.scrollIntoView({ behavior: "smooth", block: "end" });
+  }
+});
+
+document.addEventListener("scroll", () => {
+  if (!ticking && isAutoScrollOn && window.scrollY < lastKnownScrollPosition) {
+    window.requestAnimationFrame(() => {
+      isAutoScrollOn = false;
+      ticking = false;
+    });
+    ticking = true;
+  } else if (
+    !ticking &&
+    !isAutoScrollOn &&
+    window.scrollY > lastKnownScrollPosition &&
+    window.scrollY >=
+      document.documentElement.scrollHeight - window.innerHeight - 30
+  ) {
+    window.requestAnimationFrame(() => {
+      isAutoScrollOn = true;
+      ticking = false;
+    });
+    ticking = true;
+  }
+  lastKnownScrollPosition = window.scrollY;
+});
+
+//
+// make response available for copying to clipboard
+//
+function copyTextToClipboard(responseDiv) {
+  let elem = responseDiv;
+  const copyButton = document.createElement("button");
+  copyButton.className = "btn btn-secondary copy-button";
+  copyButton.innerHTML = clipboardIcon;
+  elem = copyButton;
+  elem.onclick = () => {
+    navigator.clipboard.writeText(responseDiv.innerText);
+  };
+  responseDiv.appendChild(elem);
+}
+
+//
+// user hits send, enter or ctl enter
+//
+async function submitRequest(e) {
+  if (sendButton.innerHTML == "Stop") {
+    Abort();
+    return;
+  }
+
+  // enter clears the chat history, ctl enter will continue the conversation
+  const continuation = e.ctrlKey && e.key === "Enter";
+
+  document.getElementById("chat-container").style.display = "block";
+
+  let input = document.getElementById("user-input").value;
+  if (input.length == 0) {
+    document.getElementById("chat-history").context = "";
+    let chatHistory = document.getElementById("chat-history");
+    while (chatHistory.firstChild) {
+      chatHistory.firstChild.remove();
+    }
+    return;
+  }
+  let context = document.getElementById("chat-history").context;
+  if (context === undefined) {
+    context = "";
+  }
+
+  // append to chat history
+  let chatHistory = document.getElementById("chat-history");
+  let userMessageDiv = document.createElement("div");
+  userMessageDiv.className = "mb-2 user-message";
+  userMessageDiv.innerText = input;
+  chatHistory.appendChild(userMessageDiv);
+
+  // container for llm response
+  let responseDiv = document.createElement("div");
+  responseDiv.className = "response-message mb-2 text-start";
+  responseDiv.style.minHeight = "3em";
+  let spinner = document.createElement("div");
+  spinner.className = "spinner-border text-light";
+  spinner.setAttribute("role", "status");
+  responseDiv.appendChild(spinner);
+  chatHistory.appendChild(responseDiv);
+
+  // toggle button to stop text generation
+  sendButton.innerHTML = "Stop";
+
+  // change autoScroller to keep track of our new responseDiv
+  autoScroller.observe(responseDiv);
+
+  if (continuation) {
+    input = context + " " + input;
+  }
+
+  Query(continuation, input, (word) => {
+    responseDiv.innerHTML = marked.parse(word);
+  })
+    .then(() => {
+      chatHistory.context = responseDiv.innerHTML;
+      copyTextToClipboard(responseDiv, true);
+      sendButton.innerHTML = "Send";
+      spinner.remove();
+    })
+    .catch((error) => {
+      console.error(error);
+      sendButton.innerHTML = "Send";
+      spinner.remove();
+    });
+
+  // Clear user input
+  document.getElementById("user-input").value = "";
+}
+
+//
+// event listener for Ctrl+Enter or Enter
+//
+document.getElementById("user-input").addEventListener("keydown", function (e) {
+  if (e.ctrlKey) {
+    if (e.key === "Enter") {
+      submitRequest(e);
+    } else {
+      const query = preCannedQueries[e.key];
+      if (query) {
+        document.getElementById("user-input").value = query;
+        submitRequest(e);
+      }
+    }
+  } else if (e.key === "Enter") {
+    e.preventDefault();
+    submitRequest(e);
+  }
+});
+
+window.onload = () => {
+  Init().then(() => {
+    // adjustPadding();
+    sendButton.addEventListener("click", submitRequest);
+    const userInput = document.getElementById("user-input");
+    document.getElementById("status").style.display = "none";
+    userInput.focus();
+  });
+};
---
+++ on-device-ai/check_coc.js
@@ -0,0 +1,125 @@
+import { Init, Query, Abort } from "./main.js";
+
+const CoC = `
+Participation in the community must be a harassment-free experience for everyone, regardless of age, body size, visible or invisible disability, ethnicity, sex characteristics, gender identity and expression, level of experience, education, socio-economic status, nationality, personal appearance, race, caste, color, religion, or sexual identity and orientation.
+
+Interactions must happen in ways that contribute to an open, welcoming, diverse, inclusive, and healthy community.
+
+Examples of behavior that contributes to a positive environment for our community include:
+
+- Demonstrating empathy and kindness toward other people
+- Being respectful of differing opinions, viewpoints, and experiences
+- Giving and gracefully accepting constructive feedback
+- Accepting responsibility and apologizing to those affected by our mistakes, and learning from the experience
+- Focusing on what is best not just for us as individuals, but for the overall community
+
+Examples of unacceptable behavior include:
+
+- The use of sexualized language or imagery, and sexual attention or advances of any kind
+- Trolling, insulting or derogatory comments, and personal or political attacks
+- Public or private harassment
+- Disruptive behavior
+- Publishing others' private information, such as a physical or email address, without their explicit permission
+- Other conduct which could reasonably be considered inappropriate in a professional setting
+`;
+
+const PROMPT = `
+<|CODE OF CONDUCT|>
+${CoC}
+<|END|>
+
+<|COMMENT|>
+{
+  "comment": "Thank you for this project, very useful to me!",
+  "isAcceptableBasedOnCodeOfConduct": true
+}
+<|END|>
+
+<|COMMENT|>
+{
+  "comment": "I found a bug in the code, when I try to run the code in Edge, it doesn't work."
+  "isAcceptableBasedOnCodeOfConduct": true
+}
+<|END|>
+
+<|COMMENT|>
+{
+  "comment": "What is this pile of garbage code? It doesn't work at all!"
+  "isAcceptableBasedOnCodeOfConduct": false,
+  "reason": "The comment is disrespectful and derogatory."
+}
+<|END|>
+
+<|COMMENT|>
+{
+  "comment": "[COMMENT]",
+  "isAcceptableBasedOnCodeOfConduct": `;
+
+const descriptionEl = document.getElementById("description");
+const form = document.getElementById("form");
+const button = document.getElementById("submit-button");
+const message = document.getElementById("message");
+
+let isCheckingComment = false;
+
+function showAsLoading() {
+  displayMessage();
+  button.textContent = "Checking your comment ...";
+  button.classList.add("loading");
+  button.disabled = true;
+}
+
+function showAsNormal() {
+  button.textContent = "Submit new issue";
+  button.classList.remove("loading");
+  button.disabled = false;
+}
+
+function displayMessage(str) {
+  message.textContent = str;
+}
+
+async function startApp() {
+  await Init();
+
+  form.onsubmit = (e) => {
+    if (isCheckingComment) {
+      return;
+    }
+
+    showAsLoading();
+    isCheckingComment = true;
+
+    e.preventDefault();
+    const prompt = PROMPT.replace("[COMMENT]", descriptionEl.value);
+    let data = "";
+    Query(true, prompt, (word) => {
+      data = word;
+      console.log(word);
+      if (word.includes("<|END|>")) {
+        Abort();
+      }
+    }).then(() => {
+      try {
+        const result = JSON.parse(
+          '{"isAcceptableBasedOnCodeOfConduct":' + data.substring(0, data.indexOf("<|END|>"))
+        );
+  
+        if (!result.isAcceptableBasedOnCodeOfConduct) {
+          displayMessage(result.reason + " Please review the Code of Conduct and modify your comment accordingly.");
+        } else {
+          displayMessage();
+        }
+      } catch (e) {
+        displayMessage();
+      }
+      
+      showAsNormal();
+      isCheckingComment = false;
+    });
+  };
+}
+
+window.onload = () => {
+  startApp();
+};
