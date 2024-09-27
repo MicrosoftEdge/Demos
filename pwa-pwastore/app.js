106a96679ed24002d0d4657619ba0fe9f725f4da
Amanda Baker <ambake@microsoft.com>
2024-09-27 16:57:00 -0700

    Remove extra whitespace

--- pwa-pwastore/app.js
+++ pwa-pwastore/app.js
@@ -3,7 +3,6 @@ const pwinterBtn = document.getElementById("installPwinter");
 const pwampBtn = document.getElementById("installPwamp");
 const bubbleBtn = document.getElementById("installBubble");
 
-
 pwinterBtn.addEventListener("click", () => {
   navigator.install("https://diek.us/pwinter/index.html?randomize=true",
                     "https://diek.us/pwinter/")
