e072667f8841592979b4da1d82943ee74e723460
Patrick Brosset <patrick.brosset@microsoft.com>
2025-07-17 15:28:02 +0200

    network reference demo

---
+++ devtools-network-reference/README.md
@@ -0,0 +1,5 @@
+# DevTools Network tool reference Demo
+
+➡️ **[Open the demo](https://microsoftedge.github.io/Demos/devtools-network-reference/)** ⬅️
+
+This is the source code for a demo page that's used in the [Network features reference](https://learn.microsoft.com/microsoft-edge/devtools/network/reference) article in the Microsoft Edge DevTools documentation.
---
+++ devtools-network-reference/script.js
@@ -0,0 +1,53 @@
+const xhrButton = document.getElementById("xhr");
+const iframesButton = document.getElementById("iframes");
+const iframesPlaceholderEl = document.getElementById("iframes-placeholder");
+const streamSseButton = document.getElementById("stream-sse");
+const formDataButton = document.getElementById("form-data");
+
+xhrButton.addEventListener("click", () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "./data.json");
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log("XHR Response:", JSON.parse(xhr.responseText));
+    } else {
+      console.error("XHR Error:", xhr.statusText);
+    }
+  };
+  xhr.onerror = () => {
+    console.error("XHR Request failed");
+  };
+  xhr.send();
+});
+
+iframesButton.addEventListener("click", () => {
+  [
+    "../detached-elements/",
+    "../demo-to-do/",
+    "../edit-context/"
+  ].forEach(url => {
+    const iframe = document.createElement("iframe");
+    iframe.src = url;
+    iframesPlaceholderEl.appendChild(iframe);
+  });
+});
+
+streamSseButton.addEventListener("click", () => {
+  const eventSource = new EventSource('https://network-tool-sse-demo.netlify.app/sse');
+
+  eventSource.addEventListener('message', event => {
+    console.log(event.data);
+  }, false);
+});
+
+formDataButton.addEventListener("click", async () => {
+  const formData = new FormData();
+  formData.append("username", "Danas Barkus");
+  formData.append("timestamp", new Date().toISOString());
+  formData.append("file", new Blob(["Hello World"], { type: "text/plain" }), "hello.txt");
+  
+  await fetch("./form-data-endpoint", {
+    method: "POST",
+    body: formData
+  });
+});
