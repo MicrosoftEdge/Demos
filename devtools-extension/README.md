378d047d208ee159a98d848e0757b4af3c2be763
Michael Hoffman <45407486+mikehoffms@users.noreply.github.com>
2025-10-27 16:18:21 -0700

    redesign

--- devtools-extension/README.md
+++ devtools-extension/README.md
@@ -1,5 +1,5 @@
 # Custom DevTools extension
 
-This sample adds a custom tool tab and panel in Microsoft Edge DevTools.
+This sample adds a custom tool in Microsoft Edge DevTools, including the tool's tab and panel.
 
-For instructions, see [Sample: Custom DevTools tool tab and panel](https://learn.microsoft.com/microsoft-edge/extensions/developer-guide/devtools-extension).
+For instructions, see [Sample: Custom DevTools tool (tab and panel)](https://learn.microsoft.com/microsoft-edge/extensions/developer-guide/devtools-extension).
--- devtools-extension/devtools.js
+++ devtools-extension/devtools.js
@@ -1,27 +1,42 @@
 let availableMemoryCapacity;
 let totalMemoryCapacity;
-let youClickedOn; 
-chrome.devtools.panels.create("Sample Panel", "icon.png", "panel.html", panel => {
+let youClickedOn;
+
+chrome.devtools.panels.create("Custom", "icon.png", "panel.html", panel => {
     // code invoked on panel creation
     panel.onShown.addListener( (extPanelWindow) => {
+        // memory
+        availableMemoryCapacity = extPanelWindow.document.querySelector('#availableMemoryCapacity');
+        totalMemoryCapacity = extPanelWindow.document.querySelector('#totalMemoryCapacity');
+        // 2-way message sending
         let sayHello = extPanelWindow.document.querySelector('#sayHello');
         youClickedOn = extPanelWindow.document.querySelector('#youClickedOn');
         sayHello.addEventListener("click", () => {
             // show a greeting alert in the inspected page
-            chrome.devtools.inspectedWindow.eval('alert("Hello from the DevTools Extension");');
+            chrome.devtools.inspectedWindow.eval('alert("Hello from the DevTools extension!");');
         });
-        // display memory
-        availableMemoryCapacity = extPanelWindow.document.querySelector('#availableMemoryCapacity');
-        totalMemoryCapacity = extPanelWindow.document.querySelector('#totalMemoryCapacity');
     });
 });
 
+// Update Memory display
+setInterval(() => {
+    chrome.system.memory.getInfo((data) => {
+        if (availableMemoryCapacity) {
+            availableMemoryCapacity.innerHTML = data.availableCapacity;
+        }
+        if (totalMemoryCapacity) {
+            totalMemoryCapacity.innerHTML = data.capacity;
+        }
+    });
+}, 1000);
+
+// Send message from inspected page to DevTools
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // Messages from content scripts should have sender.tab set
     if (sender.tab && request.click == true) {
         console.log('I am here!');
         if (youClickedOn) {
-            youClickedOn.innerHTML = `You clicked on position (${request.xPosition}, ${request.yPosition}) in the inspected page.`;
+            youClickedOn.innerHTML = `(${request.xPosition}, ${request.yPosition})`;
         }
         sendResponse({
             xPosition: request.xPosition,
@@ -40,15 +55,3 @@ backgroundPageConnection.postMessage({
     name: 'init',
     tabId: chrome.devtools.inspectedWindow.tabId
 });
-
-// Periodically update Memory display
-setInterval(() => {
-    chrome.system.memory.getInfo((data) => {
-        if (availableMemoryCapacity) {
-            availableMemoryCapacity.innerHTML = data.availableCapacity;
-        }
-        if (totalMemoryCapacity) {
-            totalMemoryCapacity.innerHTML = data.capacity;
-        }
-    });
-}, 1000);
